71cff979346636dafb38046b85edf6ae9c14485a
DeckerSU <deckersu@protonmail.com>
2025-03-08 06:19:44 +0100

    komodo_connectblock: fix hwmheight variable shadowing
    See:
    
    https://github.com/KomodoPlatform/komodo/commit/e46aae1bef0a0745ca7ffc7a9458bc04a0447de7

--- src/komodo.cpp
+++ src/komodo.cpp
@@ -680,7 +680,6 @@ void clear_fp_stateupdate() { fp = nullptr; } // tests should clear fp, before n
 
 int32_t komodo_connectblock(bool fJustCheck, CBlockIndex *pindex,CBlock& block)
 {
-    static int32_t hwmheight;
     int32_t staked_era; static int32_t lastStakedEra;
     std::vector<int32_t> notarisations;
     uint64_t signedmask,voutmask; char symbol[KOMODO_ASSETCHAIN_MAXLEN],dest[KOMODO_ASSETCHAIN_MAXLEN]; struct komodo_state *sp;
