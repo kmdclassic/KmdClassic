2eae3fd72c011134d6bc99f9f43efa517a736d57
kmdclassic <kmdclassic@pm.me>
2025-12-07 16:16:58 +0100

    refactor(wallet): Allow sapling shielded transactions for KMD in GUI and RPC

--- src/wallet/rpcwallet.cpp
+++ src/wallet/rpcwallet.cpp
@@ -4517,8 +4517,6 @@ UniValue z_sendmany(const UniValue& params, bool fHelp, const CPubKey& mypk)
                     if ( fromSprout || toSprout )
                         throw JSONRPCError(RPC_INVALID_PARAMETER,"Sprout usage has expired");
                 }
-                if ( toSapling && chainName.isKMD() )
-                    throw JSONRPCError(RPC_INVALID_PARAMETER,"Sprout usage will expire soon");
 
                 // If we are sending from a shielded address, all recipient
                 // shielded addresses must be of the same type.
